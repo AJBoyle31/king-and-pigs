c61a511109e725d203aa8d8f941a92a8c8739676
Andrew <ajboyle31@gmail.com>
2021-03-12 19:27:02 -0500

    fixed player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -24,32 +24,18 @@ onready var animations = $Animations
 onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
-	
 	match state:
 		MOVE:
 			move_state(delta)
-		RUN:
-			animationPlayer.play("Run")
-		JUMP:
-			animationPlayer.play("Jump")
-		FALL:
-			animationPlayer.play("Fall")
 		ATTACK:
 			animationPlayer.play("Attack")
 		HURT:
 			pass
 	
-	is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
-	if is_jump_interrupted:
-		state = FALL
-	
-	
-	if Input.is_action_just_pressed("jump"):
-		state = JUMP
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
-	
-	
+
+
 
 func get_direction() -> Vector2:
 	return Vector2(
@@ -71,33 +57,38 @@ func calculate_move_velocity(
 		velocity.y = 0.0
 	return velocity
 
-
+func move_state(delta):
+	is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction = get_direction()
+	if direction != Vector2.ZERO:
+		_previous_velocity = direction
+		_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	else:
+		_velocity = calculate_move_velocity(_velocity, Vector2.ZERO, Vector2.ZERO, false)
+	_velocity.y += GRAVITY * delta
+	move()
 
 func move():
+	var snap: Vector2 = Vector2.DOWN * 60.0 if _velocity.y == 0.0 else Vector2.ZERO
+	_velocity = move_and_slide_with_snap(_velocity, snap, FLOOR_NORMAL, true)
+	playAnimation(_velocity)
 	if _previous_velocity.x < 0:
 		current_animation.flip_h = true
 		current_animation.offset.x = -7
 	elif _previous_velocity.x > 0:
 		current_animation.flip_h = false
 		current_animation.offset.x = 7
-	var snap: Vector2 = Vector2.DOWN * 60.0 if _velocity.y == 0.0 else Vector2.ZERO
-	
-	_velocity = move_and_slide_with_snap(_velocity, snap, FLOOR_NORMAL, true)
 
 
-func move_state(delta):
-	var direction = get_direction()
-	if direction != Vector2.ZERO:
-		_previous_velocity = direction
+func playAnimation(velocity):
+	if velocity.y > 0:
+		animationPlayer.play("Fall")
+	elif velocity.y < 0:
+		animationPlayer.play("Jump")
+	elif velocity.x > 0 or velocity.x < 0 and is_on_floor():
 		animationPlayer.play("Run")
-		_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-		
-	else:
+	elif velocity.x == 0:
 		animationPlayer.play("Idle")
-		_velocity = calculate_move_velocity(_velocity, Vector2.ZERO, Vector2.ZERO, false)
-	_velocity.y += GRAVITY * delta
-	move()
-
 
 
 func _change_animation(state: String) -> void:
